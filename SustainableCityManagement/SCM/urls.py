402bbe17e320d087775e39eeeb1e003cc57c35c2
Rohan Bhutani <rohanbhutani1993@gmail.com>
2019-11-23 18:46:10 +0000

    Pollution api integration

--- SustainableCityManagement/SCM/urls.py
+++ SustainableCityManagement/SCM/urls.py
@@ -2,11 +2,13 @@ from django.urls import path
 from SCM.views import (
     login_view,
     ValidateLoginView,
-    DisplayMapView
+    DisplayMapView,
+    DisplayPollutionData
     )
 
 app_name = "SCM"
 urlpatterns = [
     path('home/', ValidateLoginView, name='validate-login'),
-    path('traffic/', DisplayMapView, name='display-map')
+    path('traffic/', DisplayMapView, name='display-map'),
+    path('pollution', DisplayPollutionData, name='display-pollution')
 ]
--- SustainableCityManagement/SCM/views.py
+++ SustainableCityManagement/SCM/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import UserDetails
 from .forms import UserLoginForm
+import requests
 # Create your views here.
 def login_view(request):
     form = UserLoginForm(request.POST or None)        
@@ -22,6 +23,23 @@ def ValidateLoginView(request):
 
 def DisplayMapView(request):
     return render(request,"home.html",{})
+
+
+def DisplayPollutionData(request):
+    response = requests.get('http://erc.epa.ie/real-time-air/www/aqindex/aqih_json.php')
+    pollution = response.json()
+    dict1 = dict()
+    for i in range(0, len(pollution['aqihsummary'])):
+        if '_' in pollution['aqihsummary'][i]['aqih-region']:
+            region = pollution['aqihsummary'][i]['aqih-region'].split('_')[0] + " " + pollution['aqihsummary'][i]['aqih-region'].split('_')[1]
+        else:
+            region = pollution['aqihsummary'][i]['aqih-region']
+        dict1[region] = pollution['aqihsummary'][i]['aqih']
+        
+    context = {
+        "data" : dict1
+        }
+    return render(request, "pollution.html", context)
 
     
 
