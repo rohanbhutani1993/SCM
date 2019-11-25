4ba658e6083e48bd49dc34206c8198a0f733dd83
Rohan Bhutani <rohanbhutani1993@gmail.com>
2019-11-25 14:58:56 +0000

    User login logic change

--- SustainableCityManagement/SCM/views.py
+++ SustainableCityManagement/SCM/views.py
@@ -16,8 +16,8 @@ def login_view(request):
 def ValidateLoginView(request):
     userName = request.POST['userName']
     password = request.POST['password']
-    userLoginDetails = UserDetails.objects.get()
-    if userLoginDetails.userName == userName and userLoginDetails.Password == password:
+    userLoginDetails = UserDetails.objects.filter(userName = userName, Password = password)
+    if userLoginDetails:
                return render(request,"dashboard.html",{})
     else:
         return HttpResponse("Invalid username or password.")
